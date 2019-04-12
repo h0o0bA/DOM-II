ef9816b6c353eaf8a0cda8b9f052ade390e2e419
Ihab Ali <ihab@live.com>
2019-04-12 07:06:44 -0500

    added event listeners to index.js: click, mouseover, dblclick, resize

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+const destinationBtn = document.querySelectorAll('.destination .btn');
+
+for(let i =0;i<destinationBtn.length;i++){
+    destinationBtn[i].addEventListener('click', (event) => {
+        event.target.style.backgroundColor = 'darkgreen';
+    });
+
+    destinationBtn[i].addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'green';
+    });
+
+    destinationBtn[i].addEventListener('dblclick', (event) => {
+        event.target.style.backgroundColor = 'black';
+    });
+}
+
+window.addEventListener('resize', evt => {
+    alert("Why are you resizing me!");
+});
